b5ab188457f0170c80f27591cfe82c1086de67d7
madyisus <gonzalez.29la@gmail.com>
2023-04-30 21:51:16 -0400

    add comments

--- main.cpp
+++ main.cpp
@@ -12,12 +12,14 @@ int main(int argv, char* argc[]) {
   int n, m, k, N, n_1, i, num_of_experiments;
   string selected_algorithm;
 
+// Se elije un algoritmo de acuerdo al indice ingresado en la terminal
   switch(atoi(argc[1])){
     case 3: selected_algorithm = "standard_mm"; break;
     case 2: selected_algorithm = "transpose_mm"; break;
     default: selected_algorithm = "strassen_mm"; break;
   }
 
+// Si el 3er argumento es --test, realiza la multiplicacion sobre el input dado
   if((argv > 2) && (strcmp(argc[2],"--test") == 0)){
     cin>>n>>m>>k;
     vector<vector<int> > M_A(n,vector<int>(m,0)), M_B(m,vector<int>(k,0));
@@ -29,14 +31,16 @@ int main(int argv, char* argc[]) {
     return 0;
   }
 
+//-------Esta sección realiza la medición de tiempo para n cada vez mayor sobre matrices de n x n----------
+  
   string outfile_name = selected_algorithm + "_results.csv";
   ofstream outfile(outfile_name);
   string column_names = "n,time[ms]\n";
   outfile << column_names;
 
-  i = 10, n_1 = 1, N = 400, num_of_experiments = 4;
+  i = 10, n_1 = 1, N = 400, num_of_experiments = 10;
 
-// MEDICION DEL TIEMPO EN CADA ALGORITMO
+// Se mide el tiempo de ejecución promedio para el algoritmo seleccionado
   for(int n = n_1; n <= N; n += i){
     cout<<n<<endl;
     double mm_total_time = 0;
